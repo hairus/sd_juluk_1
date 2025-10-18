356d5ab9be62fe3020186b9560d196379bd3b195
hairus <zhomenep@gmail.com>
2025-10-18 17:18:14 +0700

    tatak parjuga

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ config('app.name') }}</title>
+    <title>TATAK PARJUGA</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
     <!-- Bootstrap 3.3.7 -->
@@ -29,7 +29,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="{{ url('/') }}"><b>Siakad Parjughe</b> <br>SD JULUK 1</a>
+        <a href="{{ url('/') }}"><b>Siakad Tatak Parjuga</b> <br>SDN JULUK 1</a>
     </div>
 
     <div class="login-box-body">
--- resources/views/guru/getKeg.blade.php
+++ resources/views/guru/getKeg.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <label>Pilih Kegiatan</label><br>
+    <label>Pilih Kegiatan Min 3 Kegiatan</label><br>
     @foreach($kegs as $data )
         <div>
             <input type="checkbox" name="row[]" value="{{ $data->id }}"> {{ $data->kegiatan }}
